afbc3106979b3cf0aeecfce1977160d60ce475d3
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-11-22 10:30:22 -0800

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-let text = document.getElementById("text");
+let eyeicon = document.getElementById("eyeIcon");
+let pass = document.getElementById("password");
+function showHide() {
+  if (pass.type == "password") {
+    pass.type = "text";
+    eyeIcon.src = "eyeOpen.png";
+  } else {
+    pass.type = "password";
+    eyeIcon.src = "eyeClose.png";
+  }
+}
+let email = document.getElementById("email");
 let password = document.getElementById("password");
+let form = document.getElementById("form");
+function sub() {
+  let text = document.getElementById("text").value;
+  document.write(text.value);
+}
